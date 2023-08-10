ef87203245a8c659ae626394fa221596548a6999
FARHOD MUHAMADIEV <farmukh@inbox.ru>
2023-08-10 10:06:46 +0500

    removed logs

--- service-one/index.js
+++ service-one/index.js
@@ -1,81 +1,84 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4001;
-const expressWinston = require('express-winston')
-// const { transports, format } = require('winston')
+const expressWinston = require('express-winston');
 
-const logger = require('./logger')
+const logger = require('./logger');
 
-
-app.use(expressWinston.logger({
+app.use(
+  expressWinston.logger({
     winstonInstance: logger,
-    statusLevels: true
-}))
+    statusLevels: true,
+  }),
+);
 
 app.use(express.json());
 
 var amqp = require('amqplib');
 
-var channel, connection; 
+var channel, connection, q;
 connectQueue();
 async function connectQueue() {
-    try {
-        connection = await amqp.connect('amqp://localhost:5672');
-        channel = await connection.createChannel();
-        
-        await channel.assertQueue('test-queue');
-        logger.info("channel created successfully");
-    } catch (error) {
-        logger.error(error);
-    }
+  try {
+    connection = await amqp.connect('amqp://localhost:5672');
+    channel = await connection.createChannel();
+
+    q = await channel.assertQueue('', { exclusive: true });
+    logger.info('temp channel created successfully');
+  } catch (error) {
+    logger.error(error);
+  }
 }
 
 function generateUuid() {
-    return Math.random().toString() +
-            Math.random().toString() +
-            Math.random().toString();
+  return Math.random().toString() + Math.random().toString() + Math.random().toString();
 }
 
 const sendData = async (data) => {
-    return new Promise((resolve, reject) => {
-        try {
-            var resp, correlationId = generateUuid();
-            channel.sendToQueue('test-queue', Buffer.from(JSON.stringify(data)), {
-                correlationId: correlationId,
-                replyTo: "test-queue"
-            });
-            logger.info("message sent successfully");
+  return new Promise((resolve, reject) => {
+    try {
+      var resp,
+        correlationId = generateUuid();
 
-            channel.consume("test-queue", function (msg) {
-                if (msg.properties.correlationId == correlationId) {
+      channel.sendToQueue('test-queue', Buffer.from(JSON.stringify(data)), {
+        correlationId: correlationId,
+        replyTo: q.queue,
+      });
+      logger.info('message sent successfully');
 
-                    setTimeout(function () {
-                        connection.close();
-                        process.exit(0)
-                    }, 500);
+      channel.consume(
+        q.queue,
+        function (msg) {
+          // console.log(msg.content.toString())
+          if (msg.properties.correlationId == correlationId) {
+            setTimeout(function () {
+              connection.close();
+              process.exit(0);
+            }, 500);
 
-                    resp = msg.content.toString();
-                    logger.info("response received");
-                    resolve(resp);
-                }
-            }, {
-                noAck: true
-            });
-        } catch (error) {
-            logger.error(error);
-            reject(error);
-        }
-    })
-}
+            resp = msg.content.toString();
+            logger.info('response received');
+            resolve(resp);
+          }
+        },
+        {
+          noAck: true,
+        },
+      );
+    } catch (error) {
+      logger.error(error);
+      reject(error);
+    }
+  });
+};
 
-app.post("/multiply", async function(req, res) {
-    let resp = await sendData(req.body.num);
-    logger.info("A message is sent to queue")
+app.post('/multiply', async function (req, res) {
+  let resp = await sendData(req.body.num);
+  logger.info('A message is sent to queue');
 
-    res.send("Number multiplied by 5 => "+resp)
-})
+  res.send('Number multiplied by 5 => ' + resp);
+});
 
 app.listen(PORT, () => {
-    console.log("Server running at port " + PORT)
-    logger.info("Server running at port " + PORT)
+  logger.info('Server running at port ' + PORT);
 });
--- service-two/index.js
+++ service-two/index.js
@@ -4,7 +4,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 4002;
 const expressWinston = require('express-winston')
-// const { transports, format } = require('winston')
 
 const logger = require('./logger')
 
@@ -21,25 +20,28 @@ connectQueue()
 async function connectQueue() {
     try {
         connection = await amqp.connect("amqp://localhost:5672");
-        channel    = await connection.createChannel()
+        channel    = await connection.createChannel();
         
-        await channel.assertQueue("test-queue")
+        await channel.assertQueue("test-queue", {durable: true });
+        channel.prefetch(1);
         console.log('Awaiting RPC requests');
         logger.info('Awaiting RPC requests');
         
-        channel.consume("test-queue", data => {
-            let resp = data.content.num*5
-            channel.sendToQueue(msg.properties.replyTo, resp, {
+        channel.consume("test-queue", msg => {
+            let n = parseInt(msg.content.toString());
+            let result = n * 5;
+
+            let resultBuffer = Buffer.from(result.toString());
+            channel.sendToQueue(msg.properties.replyTo, resultBuffer, {
                 correlationId: msg.properties.correlationId
             });
-            
+            logger.info("Message sent to queue");
 
-            channel.ack(data);
-        })
+            channel.ack(msg);
+        });
     } catch (error) {
         logger.error(error);
     }
 }
 
 app.listen(PORT, () => console.log("Server running at port " + PORT));
-
